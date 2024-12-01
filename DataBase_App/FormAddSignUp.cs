c453254fc0e4e8d16f52af68ad62bf3c4e337c76
FedkinDmitriy <fedkindmitriy@yandex.ru>
2024-12-01 18:20:28 +0300

    +

--- DataBase_App/FormAddSignUp.cs
+++ DataBase_App/FormAddSignUp.cs
@@ -21,7 +21,7 @@ namespace DataBase_App
             _connection = connection;
 
             LoadClients();
-            //LoadTrainings();
+            LoadTrainings();
         }
 
         private void LoadClients()
@@ -44,13 +44,56 @@ namespace DataBase_App
             }
         }
 
+        private void LoadTrainings()
+        {
+            try
+            {
+                string query = "SELECT training_id, title FROM public.\"Training\"";
+                NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(query, _connection);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+
+                // Устанавливаем источник данных ComboBox
+                comboBox_training.DataSource = dataTable;
+                comboBox_training.DisplayMember = "title"; // Отображаемое название
+                comboBox_training.ValueMember = "training_id";  // Скрытое значение
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка загрузки тренировок: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
         // кнопка Добавить
         private void Button_addSignUp_Click(object sender, EventArgs e)
         {
+            try
+            {
 
+                int clientId = Convert.ToInt32(comboBox_client.SelectedValue);
+                int trainingId = Convert.ToInt32(comboBox_training.SelectedValue);
 
+                DateTime date = dateTimePicker1.Value;
+                bool status = checkBox_status.Checked;
 
-            this.DialogResult = DialogResult.OK;
+                string query = "INSERT INTO public.\"SignUp\" (client_id, training_id, date_registration, status_st) VALUES (@client_id, @training_id, @date, @status)";
+                using (var cmd = new NpgsqlCommand(query, _connection))
+                {
+                    cmd.Parameters.AddWithValue("@client_id", clientId);
+                    cmd.Parameters.AddWithValue("@training_id", trainingId);
+                    cmd.Parameters.AddWithValue("@date", date);
+                    cmd.Parameters.AddWithValue("@status", status);
+
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Запись успешно добавлена!", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.DialogResult = DialogResult.OK;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при добавлении записи: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
